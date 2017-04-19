b39655f3f702f773a5f95c5295e1ee4fe854930b
knowlsie <tomobknowles@gmail.com>
2017-04-19 11:27:29 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const gulp = require('gulp');
+const babili = require('gulp-babili');
+const zip = require('gulp-zip');
+const lambda = require('gulp-awslambda');
+const eslint = require('gulp-eslint');
+const clean = require('gulp-clean');
+const shell = require('gulp-shell');
+
+const params = {
+  FunctionName: 'FavouritePlaces',
+  Role: 'arn:aws:iam::{id}:role/lambda_dynamodb_execution',
+  Handler: 'index.handler',
+  Runtime: 'nodejs4.3',
+  Description: 'Demonstrates skill builder dialogue model.',
+};
+
+gulp.task('clean', function () {
+  return gulp.src('./build', { read: false })
+    .pipe(clean());
+});
+
+// This task requires an .eslintrc.json file.
+gulp.task('lint', function lint() {
+  return gulp.src(['./lambda/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('minify', ['clean'], function minify() {
+  return gulp.src('./lambda/*.js')
+    .pipe(babili())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('moveModules', ['clean'], function moveModules() {
+  return gulp.src('./lambda/node_modules/**')
+  .pipe(gulp.dest('./build/node_modules/'));
+});
+
+gulp.task('makeZip', ['moveModules', 'minify'], function makeZip() {
+  return gulp.src('./build/**')
+  .pipe(zip('lambda.zip'))
+  .pipe(gulp.dest('./build/package/'));
+});
+
+gulp.task('update', ['makeZip', 'lint'], function update() {
+  return gulp.src('./build/package/lambda.zip')
+  .pipe(lambda('FavouritePlaces', { region: 'eu-west-1' }));
+});
+
+gulp.task('create', ['makeZip', 'lint'], function create() {
+  return gulp.src('./build/package/lambda.zip')
+  .pipe(lambda(params, { region: 'eu-west-1' }))
+  .pipe(shell('aws lambda add-permission --function-name FavouritePlaces --statement-id "1234" --action "lambda:InvokeFunction" --principal "alexa-appkit.amazon.com"  --region eu-west-1'));
+});
+
+gulp.task('createRole', shell.task([
+  'aws iam create-role --role-name favourite_places_basic_execution --assume-role-policy-document file://automation/role-policy-document.json',
+  'aws iam put-role-policy --role-name favourite_places_basic_execution --policy-name lambda_basic_execution --policy-document file://automation/basic-execution-role.json',
+  'aws iam get-role --role-name favourite_places_basic_execution',
+]));
+
+gulp.task('configure', shell.task([
+  'read -p "Insert IAM ID here: " id ; aws configure set aws_access_key_id $id',
+  'read -p "Insert IAM Secret Key here: " key ; aws configure set aws_secret_access_key $key',
+  'aws configure set default.region eu-west-1',
+]));
+
+gulp.task('default', ['update']);
---
+++ lambda/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const sessionHandlers = {
+  LaunchRequest() {
+    let output;
+    if (this.attributes.favouriteCity && this.attributes.favouriteRegion) {
+      output = `Last time you told me your favourite British city was ${this.attributes.favouriteCity} and your favourite British region was ${this.attributes.favouriteRegion}.`;
+    } else {
+      output = `I don't yet have your favourite city or region. Say update my favourite places to start.`;
+    }
+    this.emit(':ask', output);
+  },
+  FavouriteIntent() {
+    if (this.event.request.dialogState !== 'COMPLETED') {
+      this.emit(':delegate');
+    } else {
+      this.attributes.favouriteCity = this.event.request.intent.slots.FavouriteCity.value;
+      this.attributes.favouriteRegion = this.event.request.intent.slots.FavouriteRegion.value;
+      this.emit(':tell', `I've recorded in my database that your favourite city is ${this.attributes.favouriteCity} and your favourite region is ${this.attributes.favouriteRegion}.`);
+    }
+  },
+  'AMAZON.HelpIntent': function HelpIntent() {
+    this.emit(':ask', 'Tell me about your favourite city and region!');
+  },
+  'AMAZON.CancelIntent': function CancelIntent() {
+    this.emit(':tell', 'Goodbye!');
+  },
+  'AMAZON.StopIntent': function StopIntent() {
+    this.emit(':tell', 'Goodbye!');
+  },
+  Unhandled() {
+    this.emit(':tell', 'There was an error.');
+  },
+};
+
+// Main
+exports.handler = (event, context, callback) => {
+  const alexa = Alexa.handler(event, context, callback); // See alexa.js in alexa-sdk package.
+  alexa.dynamoDBTableName = 'FavouritePlaces';
+  alexa.registerHandlers(sessionHandlers); // See response.js in alexa-sdk package to see other registered handlers.
+  alexa.execute(); // Handles lambda event.
+};
